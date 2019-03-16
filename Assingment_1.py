a16faa9b70f1ececd9c83e09af745f09abd51c91
U160709028 <sanderyaz@gmail.com>
2019-03-16 13:31:50 -0700

    Chroot

---
+++ Assingment_1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import os,sys
+
+#Create jail path
+os.system('mkdir /chroot')
+os.system('mkdir /chroot/bin')
+os.chdir ('/chroot')
+os.system('mkdir bin dev etc home  home/prisoner lib lib64 var usr usr/bin')
+
+#copy bash file
+os.system ('sudo cp /bin/bash /chroot/bin/')
+os.system ('sudo cp /lib64/ld-linux-x86-64.so.2 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libtinfo.so.5 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libdl.so.2 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libc.so.6 /chroot/lib/x86_64-linux-gnu')
+
+#copy python3 file
+os.system ('sudo cp /usr/bin/python3 /chroot/usr/bin/python3')
+os.system ('sudo cp /lib64/ld-linux-x86-64.so.2 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libpthread.so.0 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libc.so.6 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libdl.so.2 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libutil.so.1 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libexpat.so.1 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libz.so.1 /chroot/lib/x86_64-linux-gnu')
+os.system ('sudo cp /lib/x86_64-linux-gnu/libm.so.6 /chroot/lib/x86_64-linux-gnu')
+
+os.system ('sudo chroot /chroot /bin/bash')
+
+
+
